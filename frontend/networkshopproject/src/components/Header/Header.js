7d669174ab175daed7c9f65b7b5e7ad1c20d04f4
prsanikpour <prsanikpour1382@gmail.com>
2025-01-15 18:46:10 +0330

    head part of the header
    finished the head part of the header (loginBTN,searchBar,logo)

---
+++ frontend/networkshopproject/src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import './Header.css';
+
+
+
+
+export default function Header(){
+    return(
+        <div className="container">   
+            <div className="loginBtnSection">
+                <button>
+                    <p>ورود / ثبت نام</p>
+                    <i class="fa-solid fa-user"></i>
+                </button>
+                <a href="">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+            </div>
+            
+            <div className="searchBar">
+                <input type="search" placeholder=" جستجو"/>
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+
+            <div className="logo">
+                <img src="./l.jpg" alt="" />
+            </div>
+
+
+        </div>
+    )
+}
